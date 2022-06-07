2658973dfa69cd09dcab5dd774a897747855dee5
Tiffany Rose <tiffany.drose@gmail.com>
2022-06-07 16:46:37 -0400

    tweaking keydown event listener

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 console.log("hello world");
+
+// Initialize variabales
+let wordGuess;
+
+let wordToDisplay;
+
+let wrongLetter;
+
+// Select Elements
+
+const body = document.body;
+const wordGuessDiv = document.querySelector(".word-to-guess");
+
+body.addEventListener("keydown", (evt) => {
+  let currKey = evt.key;
+  console.log(currKey);
+});
+
+// function definitions
+
+function addWordToGuess() {
+    wordGuessDiv.innerHTML = "";
+    
+}
+
+// create an array for the word
+
+// create conditional for if the answer is right or wrong
+
+// loop through the array to
